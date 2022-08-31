3c1e83c2dda1a629e6da5989e19a7afb299090b9
unknown <gespejo2144@gmail.com>
2022-08-30 17:33:17 -0700

    added JWT authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
-
+const jwt = require('json-web-token')
 const { User } = db
 
 router.post('/', async (req, res) => {
@@ -15,7 +15,29 @@ router.post('/', async (req, res) => {
         })
     }
     else{
-        res.json({user})
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({user: user, token: result.value})
+    }
+})
+
+router.get('/profile', async(req, res) => {
+    try {
+
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        if(authenticationMethod == 'Bearer'){
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            const { id } = result.value
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+        res.json(user)
+        }
+        
+    } catch {
+        res.json(null)
     }
 })
 
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -30,6 +30,7 @@ function LoginForm() {
     //    console.log(data)
        if(response.status === 200){
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push('/')
        }
        else{
